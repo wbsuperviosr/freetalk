2c82b24a1c21b83f31eda263bd6c1efc6515705e
wbsuperviosr <rabopanjuan@gmail.com>
2023-01-07 09:35:20 +0100

    create dev branch

--- pages/sandbox.tsx
+++ pages/sandbox.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function SanBox() {
-	return <div>SanBox</div>;
+	return (
+		<div className="flex bg-black text-white justify-center">
+			<p>this is the first</p>
+			<p>this is the second</p>
+		</div>
+	);
 }
