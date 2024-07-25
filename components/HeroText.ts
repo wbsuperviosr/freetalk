1e35f57c120e50a8b576de4e2568420ba62a84a1
wbsuperviosr <rabopanjuan@gmail.com>
2024-07-25 23:06:14 +0200

    fix incorrect header image urls

--- components/HeroText.ts
+++ components/HeroText.ts
@@ -2,7 +2,7 @@ export const about_us = {
 	title: "「关于本站」",
 	text: ["虚怀乃若谷", "水深则流缓"],
 	subtext: ["关于网站的开始、现在于未来", "我们记录它茁壮成长"],
-	link: "https://assets.wbavengers.com/background_imgs/header_about.jpg",
+	link: "https://assets.wbavengers.com/Resource/background_imgs/header_about.jpg",
 };
 
 export const home_text = {
@@ -12,7 +12,7 @@ export const home_text = {
 		"我们尽可能地保存那些微弱的声音，那些隐蔽的细节",
 		"历史的夜空中，任何一粒星屑，都有它被凝视的意义",
 	],
-	link: "https://assets.wbavengers.com/background_imgs/header_index.jpg",
+	link: "https://assets.wbavengers.com/Resource/background_imgs/header_index.jpg",
 };
 export const rumor_text = {
 	title: "「江刘之案」",
@@ -21,7 +21,7 @@ export const rumor_text = {
 		"江秋莲诉刘鑫生命权纠纷案在公众平台、媒体的",
 		"重要发布，本栏目尽力保存",
 	],
-	link: "https://assets.wbavengers.com/background_imgs/header_case.png",
+	link: "https://assets.wbavengers.com/Resource/background_imgs/header_case.png",
 };
 
 export const timeline_text = {
@@ -31,14 +31,14 @@ export const timeline_text = {
 		"江秋莲诉刘鑫生命权纠纷案在公众平台、媒体的",
 		"重要发布，本栏目尽力保存",
 	],
-	link: "https://assets.wbavengers.com/background_imgs/featured.png",
+	link: "https://assets.wbavengers.com/Resource/background_imgs/featured.png",
 };
 
 export const common_voice = {
 	title: "「观者评说」",
 	text: ["那些是我人生最灰暗时刻的微光", "是我能取暖燃烧着的篝火"],
 	subtext: ["观者看法众多，并不等于本站立场", "愿君子合而不同，求同存异"],
-	link: "https://assets.wbavengers.com/background_imgs/header_watcher.jpg",
+	link: "https://assets.wbavengers.com/Resource/background_imgs/header_watcher.jpg",
 };
 
 export const nuanxi_voice = {
@@ -48,7 +48,7 @@ export const nuanxi_voice = {
 		"现在只希望网友也能通过我了解事情的另一面",
 	],
 	subtext: ["这里是自2017年5月起", "刘鑫在网上尽力留下的声音"],
-	link: "https://assets.wbavengers.com/background_imgs/header_liuxin.png",
+	link: "https://assets.wbavengers.com/Resource/background_imgs/header_liuxin.png",
 };
 
 export const media_text = {
@@ -61,12 +61,12 @@ export const media_text = {
 		"江秋莲诉刘鑫生命权纠纷案在公众平台、媒体的",
 		"重要发布，本栏目尽力保存",
 	],
-	link: "https://assets.wbavengers.com/background_imgs/header_case.jpg",
+	link: "https://assets.wbavengers.com/Resource/background_imgs/header_case.jpg",
 };
 
 export const casfile_text = {
 	title: "「部分卷宗」",
 	text: ["有缘见到的仅有这些，", "也已经说明了许多"],
 	subtext: ["2016年日本留学生被害案部分卷宗", "本栏目尽力保存"],
-	link: "https://assets.wbavengers.com/background_imgs/header_case.jpg",
+	link: "https://assets.wbavengers.com/Resource/background_imgs/header_case.jpg",
 };
